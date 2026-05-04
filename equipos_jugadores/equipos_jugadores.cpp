5d55d965c9c5fe6ac8219a6e05569400ec1f157c
Jaal-09 <jaal9709+github@gmail.com>
2026-05-04 13:05:38 -0500

    Agregado herencia de jugador y entrenador ejercicio terminado

--- equipos_jugadores/equipos_jugadores.cpp
+++ equipos_jugadores/equipos_jugadores.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 
 class EquipoFutbol{
     
@@ -10,11 +12,13 @@ class EquipoFutbol{
         int puntosTemporada;
 
     //Settters
+    public: 
+
     void setNombre(std::string nom){
         nombre = nom;
     }
 
-    void setPuntosTemporada(int puntos){
+    void setPuntos(int puntos){
         puntosTemporada = puntos;
     }
 
@@ -28,3 +32,75 @@ class EquipoFutbol{
     }
 
 };
+
+class Jugador : public EquipoFutbol {
+
+    public:
+        int golesAnotados;
+        std::string posicion;
+
+
+        void mostrarInfo(){
+            std::cout << "=== JUGADOR ===" << std::endl;
+            std::cout << "Equipo: " << getNombre() << std::endl;
+            std::cout << "Puntos: " << getPuntos() << std::endl;
+            std::cout << "Goles: " << golesAnotados << std::endl;
+            std::cout << "Posicion: " << posicion << std::endl;
+            std::cout << std::endl;
+        }
+
+};
+
+class Entrenador : public EquipoFutbol {
+    public:
+        int aniosExperiencia;
+        std::string equipoDirigido;
+
+        void mostrarInfo(){
+            std::cout << "=== ENTRENADOR ===" << std::endl;
+            std::cout << "Equipo: " << getNombre() << std::endl;
+            std::cout << "Puntos: " << getPuntos() << std::endl;
+        std::cout << "Experiencia: " << aniosExperiencia << " años" << std::endl;
+        std::cout << "Dirige a: " << equipoDirigido << std::endl;
+        std::cout << std::endl;
+        }
+};
+
+int main(){
+    srand(time(0));
+
+    //Creando objetos
+
+    Jugador jugador;
+    Entrenador entrenador;
+
+    //Asignamos valores al jugador
+    jugador.setNombre("Juventus");
+    jugador.setPuntos(rand() % 51 + 50); //50 a 100
+    jugador.golesAnotados = rand() % 31; 
+    jugador.posicion = "Delantero";
+
+    //Asignamos valores al entrenador
+    entrenador.setNombre("Milan");
+    entrenador.setPuntos(rand() % 51 + 50);
+    entrenador.aniosExperiencia = rand() % 20 + 1;
+    entrenador.equipoDirigido = "Milan";
+
+    //Mostrar la información
+    jugador.mostrarInfo();
+    entrenador.mostrarInfo();
+
+    //Comparación de puntos de entrenador vs jugador
+    std::cout << "=== COMPARACION ===" << std::endl;
+
+    if(jugador.getPuntos() > entrenador.getPuntos()){
+        std::cout << "El equipo del jugador tiene mas puntos (" << jugador.getPuntos() << " vs " << entrenador.getPuntos() << ")" << std::endl;
+    } else if(entrenador.getPuntos() > jugador.getPuntos()){
+        std::cout << "El equipo del entrenador tiene mas puntos (" << entrenador.getPuntos() << " vs " << jugador.getPuntos() << ")" << std::endl;
+    } else {
+        std::cout << "Ambos equipos tienen los mismos puntos (" << jugador.getPuntos() << ")" << std::endl;
+    }
+
+    return 0;
+}
+
